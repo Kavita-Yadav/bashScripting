c5a431e502bbb6e3f2434f3f5320fff7faf542f9
Kavita Yadav <53824088+Kavita-Yadav@users.noreply.github.com>
2022-09-30 11:05:21 +0800

    Update docker_remove.sh

--- docker_remove.sh
+++ docker_remove.sh
@@ -9,3 +9,7 @@ sudo rm -rf /var/lib/docker /etc/docker
 sudo rm /etc/apparmor.d/docker
 sudo groupdel docker
 sudo rm -rf /var/run/docker.sock
+
+#To find any potentially remaining files, you can run-> It is usefull to search all files related to any name given in cmd
+#sudo find / -name "*docker*"
+
